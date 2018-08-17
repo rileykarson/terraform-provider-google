6dc327d9cf4b63e3c316f7aceaa2b020ffd37ba0
Riley Karson <rileykarson@google.com>
2018-08-17 13:22:43 -0700

    l

---
+++ example/main.tf
@@ -0,0 +1,18 @@
+resource "google_compute_backend_bucket" "image_backend" {
+  name        = "${local.name_prefix}-backend-bucket"
+  bucket_name = "${google_storage_bucket.image_bucket.name}"
+  enable_cdn  = true
+}
+
+resource "google_storage_bucket" "image_bucket" {
+  name     = "${local.name_prefix}-bucket"
+  location = "EU"
+}
+
+resource "random_pet" "prefix" {
+  length = 2
+}
+
+locals {
+  name_prefix = "${random_pet.prefix.id}"
+}
