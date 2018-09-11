d9b8dd864c1ef35ebbd1f07323c9f606ac42333c
Riley Karson <rileykarson@users.noreply.github.com>
2018-09-11 08:14:30 -0700

    Update main.tf

--- example/main.tf
+++ example/main.tf
@@ -1,12 +1,3 @@
-locals {
-  #===
-  #  
-  # Fill in your GCP project here!
-  #
-  #===
-  project = "YOUR_PROJECT_HERE"
-}
-
 resource "google_compute_backend_bucket" "image_backend" {
   name        = "${local.name_prefix}-backend-bucket"
   bucket_name = "${google_storage_bucket.image_bucket.name}"
@@ -24,8 +15,4 @@ locals {
 
 resource "random_pet" "prefix" {
   length = 2
-}
-
-provider "google" {
-  project = "${local.project}"
 }
